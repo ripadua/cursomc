b613dbd29571cf72ed4cd2afa449b76e05905964
Ricardo Padua <ripadua7@gmail.com>
2019-06-30 11:07:09 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/ricardopadua/cursomc/domain/Produto.java
+++ src/main/java/com/ricardopadua/cursomc/domain/Produto.java
@@ -19,7 +19,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 
 	@Id
--- src/main/java/com/ricardopadua/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/ricardopadua/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,23 @@
 package com.ricardopadua.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ricardopadua.cursomc.domain.Categoria;
 import com.ricardopadua.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+	
+	@Transactional(readOnly = true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% and cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 
 }
