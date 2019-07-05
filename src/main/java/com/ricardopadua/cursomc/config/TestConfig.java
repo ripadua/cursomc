963e9a33dd2b0beeec3b1cc2b659bf095a3af567
Ricardo Padua <ripadua7@gmail.com>
2019-07-04 21:15:11 -0300

    MockEmailService com Logger. Padrões Strategy e Template

--- src/main/java/com/ricardopadua/cursomc/config/TestConfig.java
+++ src/main/java/com/ricardopadua/cursomc/config/TestConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.ricardopadua.cursomc.services.DBService;
+import com.ricardopadua.cursomc.services.EmailService;
+import com.ricardopadua.cursomc.services.MockMailService;
 
 @Configuration
 @Profile("test")
@@ -20,5 +22,10 @@ public class TestConfig {
 	public boolean instantiateDatabase() throws ParseException {
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new MockMailService();
 	}
 }
---
+++ src/main/java/com/ricardopadua/cursomc/services/EmailService.java
@@ -0,0 +1,13 @@
+package com.ricardopadua.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.ricardopadua.cursomc.domain.Pedido;
+
+public interface EmailService {
+	
+	void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+
+}
