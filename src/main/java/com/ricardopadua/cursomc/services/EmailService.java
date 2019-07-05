7fa780b869abf6bd30bd86724bc2ac5ddf1241e8
Ricardo Padua <ripadua7@gmail.com>
2019-07-04 21:59:43 -0300

    Email HTML

--- src/main/java/com/ricardopadua/cursomc/services/EmailService.java
+++ src/main/java/com/ricardopadua/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.ricardopadua.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.ricardopadua.cursomc.domain.Pedido;
@@ -9,5 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
-
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
