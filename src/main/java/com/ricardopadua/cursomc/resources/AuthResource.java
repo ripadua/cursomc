152e60cc92fb2ee44fc4c47741c29b6a9d0c6d19
Ricardo Padua <ripadua7@gmail.com>
2019-07-07 16:45:17 -0300

    Esqueci a senha

--- src/main/java/com/ricardopadua/cursomc/resources/AuthResource.java
+++ src/main/java/com/ricardopadua/cursomc/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.ricardopadua.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ricardopadua.cursomc.dto.EmailDTO;
 import com.ricardopadua.cursomc.security.JWTUtil;
 import com.ricardopadua.cursomc.security.UserSS;
+import com.ricardopadua.cursomc.services.AuthService;
 import com.ricardopadua.cursomc.services.UserService;
 
 @RestController
@@ -19,11 +23,20 @@ public class AuthResource {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
-	@RequestMapping(value="refresh_token", method=RequestMethod.POST)
+	@Autowired
+	private AuthService service;
+	
+	@RequestMapping(value="/refresh_token", method=RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/forgot", method=RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 
